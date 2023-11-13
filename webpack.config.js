0c7240924b184bcfef0fe4d165ea8fff84caebb5
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-11-13 13:12:24 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = (env) => {
+	const { mode } = env;
+	const devMode = mode === "development";
+
+	return {
+		mode: mode || "development",
+		entry: "./src/index.js",
+		output: {
+			filename: "main.js",
+			path: path.resolve(__dirname, "dist"),
+			clean: true,
+		},
+		devtool: "inline-source-map",
+		plugins: [
+			new HtmlWebpackPlugin({ template: "public/index.html" }),
+			new MiniCssExtractPlugin({
+				filename: devMode ? "[name].css" : "[name].[contenthash].css",
+			}),
+			// new CopyPlugin({
+			// 	patterns: [{ from: "public/icons", to: "icons" }],
+			// }),
+			new BrowserSyncPlugin({
+				host: "localhost",
+				port: 3000,
+				proxy: "http://localhost:8080/",
+				open: false,
+			}),
+			new webpack.ProgressPlugin(),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.scss$/i,
+					use: [
+						mode === "production"
+							? MiniCssExtractPlugin.loader
+							: "style-loader",
+						"css-loader",
+						"sass-loader",
+					],
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: "asset/resource",
+				},
+			],
+		},
+		resolve: {
+			modules: [path.resolve(__dirname, "public/img"), "node_modules"],
+		},
+	};
+};
