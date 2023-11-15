f2e4b4e06dc2466ac067db7ee27456ba7feb33d2
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-11-15 14:27:55 +0300

    finished section features

--- webpack.config.js
+++ webpack.config.js
@@ -6,54 +6,54 @@ const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = (env) => {
-	const { mode } = env;
-	const devMode = mode === "development";
+  const { mode } = env;
+  const devMode = mode === "development";
 
-	return {
-		mode: mode || "development",
-		entry: "./src/index.js",
-		output: {
-			filename: "main.js",
-			path: path.resolve(__dirname, "dist"),
-			clean: true,
-		},
-		devtool: "inline-source-map",
-		plugins: [
-			new HtmlWebpackPlugin({ template: "public/index.html" }),
-			new MiniCssExtractPlugin({
-				filename: devMode ? "[name].css" : "[name].[contenthash].css",
-			}),
-			// new CopyPlugin({
-			// 	patterns: [{ from: "public/icons", to: "icons" }],
-			// }),
-			new BrowserSyncPlugin({
-				host: "localhost",
-				port: 3000,
-				proxy: "http://localhost:8080/",
-				open: false,
-			}),
-			new webpack.ProgressPlugin(),
-		],
-		module: {
-			rules: [
-				{
-					test: /\.scss$/i,
-					use: [
-						mode === "production"
-							? MiniCssExtractPlugin.loader
-							: "style-loader",
-						"css-loader",
-						"sass-loader",
-					],
-				},
-				{
-					test: /\.(png|svg|jpg|jpeg|gif)$/i,
-					type: "asset/resource",
-				},
-			],
-		},
-		resolve: {
-			modules: [path.resolve(__dirname, "public/img"), "node_modules"],
-		},
-	};
+  return {
+    mode: mode || "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devtool: "inline-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({ template: "public/index.html" }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? "[name].css" : "[name].[contenthash].css",
+      }),
+      new CopyPlugin({
+        patterns: [{ from: "public/icons", to: "icons" }],
+      }),
+      new BrowserSyncPlugin({
+        host: "localhost",
+        port: 3000,
+        proxy: "http://localhost:8080/",
+        open: false,
+      }),
+      new webpack.ProgressPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            mode === "production"
+              ? MiniCssExtractPlugin.loader
+              : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, "public/img"), "node_modules"],
+    },
+  };
 };
